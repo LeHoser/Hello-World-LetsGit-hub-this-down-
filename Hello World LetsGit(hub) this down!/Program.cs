d78932ab70f041e7e3fe2c6cd7d4c3442ee3e4bf
LeHoser <nathan.lloyd.7003@gmail.com>
2022-09-12 12:12:25 -0300

    Initial commit

---
+++ Hello World LetsGit(hub) this down!/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World_LetsGit_hub__this_down_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Test World");
+            Console.WriteLine("Lets commit this to memory!");
+        }
+    }
+}
